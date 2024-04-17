abbc0aa319e9a38e78f0fea8243548d8047e7d5e
pratyushb25 <1125.pratyushb@gmail.com>
2024-04-17 23:45:53 +1000

    Initial commit with folder structure

---
+++ api/flask_api.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+import pandas as pd
+from joblib import load
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Load the preprocessor and model
+preprocessor = load('preprocessor.joblib')
+model = tf.keras.models.load_model('Dragline_Factors.h5')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Extract and preprocess the input data
+        input_data = request.json
+        df = pd.DataFrame([input_data])
+        preprocessed_data = preprocessor.transform(df)
+        
+        # Make a prediction
+        prediction = model.predict(preprocessed_data)
+        
+        # Send back the prediction
+        return jsonify({'prediction': prediction.tolist()})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
