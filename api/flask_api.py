dbc58c570e33e1db9202e3cd09d77a655c23cd73
pratyushb25 <1125.pratyushb@gmail.com>
2024-04-26 23:22:24 +1000

    configuring api and webapp setup

--- api/flask_api.py
+++ api/flask_api.py
@@ -8,8 +8,11 @@ app = Flask(__name__)
 CORS(app)
 
 # Load the preprocessor and model
-preprocessor = load('preprocessor.joblib')
-model = tf.keras.models.load_model('Dragline_Factors.h5')
+preprocessor = load('../model/preprocessor.joblib')
+
+custom_objects = {'mse': tf.keras.losses.MeanSquaredError()}
+
+model = tf.keras.models.load_model('../model/Dragline_Factors.h5', custom_objects=custom_objects)
 
 @app.route('/predict', methods=['POST'])
 def predict():
